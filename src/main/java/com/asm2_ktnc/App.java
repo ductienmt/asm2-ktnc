24c2d73f89ae26ccdf3a2a16f316e37b5ae9b2f4
ductienmt <ductien.mt26@gmail.com>
2024-04-06 19:46:27 +0700

    create project

---
+++ src/main/java/com/asm2_ktnc/App.java
@@ -0,0 +1,13 @@
+package com.asm2_ktnc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
